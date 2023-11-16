ec098a8d532cb4f8aaa5e581ea90c8d402e2264a
Michel Atieno <michelatieno23@gmail.com>
2023-11-16 14:17:55 +0300

    Redefined methods for better output

--- supercli.py
+++ supercli.py
@@ -12,14 +12,16 @@ import numpy as np
                prompt="Your name, please? OR You can select one of these names to give you a preview of what to expect----> Alice, Frank, Bob, Carol, Dave\n")
 def supercli(name):
     '''
-    This prints "Hello, {name}"
+    This is a recommendation engine. You can input your name to get recommendations or use dummy data provided to test your output.
     '''     
     ##Cleaning the dataset and resetting the index
     movies = pd.read_csv("data.txt")
     movies["Clean_Rating"] = movies["Rating"].str.replace("Five", "5", regex=True)
     movies["Clean_Rating"] = movies["Clean_Rating"].str.replace(r'[^0-9,.]', '', regex=True)
-    movies = movies[movies["Clean_Rating"] != ""]   
+    ##User Victor has no ratings provided, either Nan or string value. Remove the noise from dataset
+    movies = movies[movies["User"] != "Victor"]
     movies["Clean_Rating"] = movies["Clean_Rating"].fillna(0)
+    movies["Clean_Rating"] = movies["Clean_Rating"].astype(float)
     movies = movies.reset_index(drop=True)
 
     ##Creating Search Algorithm for user and movie to take into account characters from input to optimize searches
@@ -58,15 +60,12 @@ def supercli(name):
         similar_user_recs = movies[(movies["User"].isin(users_watching_similar_movies["User"])) & (movies["Clean_Rating"] >= 3)][["Movie", "User"]]
         user_recs = similar_user_recs[(similar_user_recs["User"] != user)]["Movie"].unique()
         # similar_user_recs
-        user_recs_list = user_recs.tolist()
+        user_recs_df = pd.DataFrame(user_recs.tolist(), columns=["'s Recommended Movies"])
+        user_recs_df = user_recs_df.add_prefix(user)
         
-        return user_recs_list
-    
-    movie_list = movies["Movie"].unique()
-    movie_list = movie_list.tolist()
-    
+        return user_recs_df
    
-    def find_similar_movie(movie):
+    def find_similar_movies(movie):
         #Finding recommendations from similar users
         similar_users = movies[(movies["Movie"] == movie) & (movies["Clean_Rating"] >= 3)]["User"].unique()
         similar_user_recs = movies[(movies["User"].isin(similar_users)) & (movies["Clean_Rating"] >= 3)]["Movie"]
@@ -74,42 +73,34 @@ def supercli(name):
         #Only 10% of users
         similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
         similar_user_recs = similar_user_recs[similar_user_recs > .10]
-
-        #How common the recommendations were amongst other users
-        # all_users = movies[(movies["Movie"].isin(similar_user_recs.index)) & (movies["Clean_Rating"] >= 3)]
-        # all_user_recs = all_users["Movie"].value_counts() / len(all_users["User"].unique())
-
-        # rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
-        # rec_percentages.columns = ["similar", "all"]
-            
-        # rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
-        # rec_percentages = rec_percentages.sort_values("score", ascending=False)
-
-        return similar_user_recs
+    
+        movie_recs_df = pd.DataFrame(similar_user_recs)
+        movie_recs_df = movie_recs_df.drop(columns="count")
+        movie_recs_df.reset_index(inplace=True)
+        movie_recs_df = movie_recs_df.rename(columns = {'Movie':'Movies similar to '})
+        movie_recs_df = movie_recs_df.add_suffix(movie)
+        movie_recs_df = movie_recs_df[1:].reset_index(drop=True)
+        
+        return movie_recs_df
     
     ## Output on terminal
 
     input_name_results = search_user(name)
     user = input_name_results.iloc[0]["User"]
     if len(user_recommended_movies(name)) > 0 and len(name) > 2:
-        # print()
         click.echo(user_recommended_movies(name))
     elif len(name) < 2:
-        # print()
         click.echo(f"Length of {name} is less than 2 characters")
     elif user.lower() == name.lower():
-        # print()
         click.echo(user_recommended_movies(user))
     else:
         movie_name = click.prompt("Please select one of these movies and get recommendations that are similar ----> Star Wars,The Godfather,Titanic,The Matrix,Inception,Pulp Fiction,Forrest Gump")
         if len(movie_name) < 2:
-            # print()
             click.echo(f"Length of {movie_name} is less than 2 characters")
         else:
             results = search_movie(movie_name)
             movie_result = results.iloc[0]["Movie"]
-            # print()
-            click.echo(find_similar_movie(movie_result))
+            click.echo(find_similar_movies(movie_result))
 
 if __name__ == '__main__':
     supercli()
